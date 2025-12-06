e038d8d80665093cddf045109758196e5b15e75e
jojoapilll1-ux <jojoapilll1@gmail.com>
2025-12-06 09:07:44 +0700

    Tugas pertemuan 1 - Backend

--- Object-Array.js
+++ Object-Array.js
@@ -1,14 +1,16 @@
 // Object
 const user = {
-  name: "Sugeng",
-  age: 20,
-  hobby: "Ngegame",
+  NAMA: "APRILIVA PUTRI",
+  NIM: 25541035,
+  PRODI: "IF 1",
 };
 
 // Array
-const fruits = ["Apple", "Banana", "Orange"];
+// const fruits = ["Apple", "Banana", "Orange"];
 
-console.log(user.name);
-console.log(user.hobby);
-console.log(fruits[1]);
-console.log(fruits[0]);
+console.log(user.NAMA);
+console.log(user.NIM);
+console.log(user.PRODI);
+
+//console.log(fruits[1]);
+//console.log(fruits[0]);
